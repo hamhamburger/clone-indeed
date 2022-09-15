0893b2a5127ce2048c687cb29460ec907a6127a0
hamham <ham@example.com>
2022-09-15 22:43:12 +0900

    fix:Reactの作法に則った使い回しのできるコンポーネントに修正

--- src/components/parts/SelectableInput.tsx
+++ src/components/parts/SelectableInput.tsx
@@ -1,4 +1,6 @@
-import React, { useState,useRef,useEffect } from 'react';
+// こっちのコンポーネントはUIライブラリなしで作ったものです
+
+import React, { useState, useRef, useEffect } from 'react';
 import { css } from '@emotion/react';
 import InputWithTitle from './InputWithTitle';
 
@@ -6,93 +8,79 @@ type Props = {
   title: string;
   placeholder: string;
   children: JSX.Element;
-  list?: string[];
-  onChange: (s: string) => void;
+  options: string[];
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-
 const Style = css({
-  border: "1px solid",
-  borderRadius: "0.5rem",
-  padding: "10px",
-  position:"relative"
+  border: '1px solid',
+  borderRadius: '0.5rem',
+  padding: '10px',
+  position: 'relative',
 });
 
 const SelectableInput = ({
   title,
   placeholder,
   children,
-  list,
-  onChange
+  options,
+  onChange,
 }: Props): JSX.Element => {
   const [expanded, setExpanded] = useState<boolean>(false);
-  const [value, setValue] = useState<string>("")
+  const [value, setValue] = useState<string>('');
   const insideRef = useRef<HTMLDivElement>(null);
 
-  const onChangeChild = (e: React.ChangeEvent<HTMLInputElement>): void => setValue(e.target.value) 
-  
   useEffect(() => {
     const el = insideRef.current;
     if (el === undefined) return;
 
     const handleClickOutside = (e: MouseEvent): void => {
-      if (!((el?.contains(e.target as Node)) ?? false)) {
+      if (!(el?.contains(e.target as Node) ?? false)) {
         setExpanded(false);
-      } 
+      }
     };
     document.addEventListener('click', handleClickOutside);
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
     // ここの実装は https://hirakublog.com/react-click-outside/を参考にさせていただきました
-
-  }, [insideRef])
-  
-  useEffect(() => {
-    onChange(value)
-  
-  }, [value])
-  
+  }, [insideRef]);
 
   return (
-      <div css={Style}  ref={insideRef}>
-        <InputWithTitle
-          title={title}
-          placeholder={placeholder}
-          onSelect={() => setExpanded(true)}
-          onChange={onChangeChild}
-          valueFromParent={value}
-        >
-          {children}
-        </InputWithTitle>
-        <div className='list' css={css({position:"relative",zIndex:"2"})}>
-          {expanded ? (
-            list?.map((value) => {
-              return (
-                <div
-                  key={value}
-                  css={css({
-                    ":hover": {backgroundColor:"lightblue"}
-                  })}
-                  onClick={() => {
-                    setValue(value)
-                    setExpanded(false)
-
-                  }
-                  }
-                >
-                  {value}
-                </div>
-              );
-            })
-          ) : (
-            <></>
-          )}
-        </div>
+    <div css={Style} ref={insideRef}>
+      <InputWithTitle
+        title={title}
+        placeholder={placeholder}
+        onSelect={() => setExpanded(true)}
+        onChange={onChange}
+        valueFromParent={value}
+      >
+        {children}
+      </InputWithTitle>
+      <div className='list' css={css({ position: 'relative', zIndex: '2' })}>
+        {expanded ? (
+          options?.map((value) => {
+            return (
+              <div
+                key={value}
+                css={css({
+                  ':hover': { backgroundColor: 'lightblue' },
+                })}
+                onClick={() => {
+                  setValue(value);
+                  setExpanded(false);
+                }}
+              >
+                {value}
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
       </div>
+    </div>
   );
 };
 
 export default SelectableInput;
-
-
