dbea212facbc6db67ac0ee58ec1723f42b274375
hamham <ham@example.com>
2022-09-15 22:47:38 +0900

    SelectBox完成

---
+++ src/components/parts/SelectBox.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {MenuItem,Select,FormControl,InputLabel} from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
+
+type Props = {
+  onChange: (event: SelectChangeEvent<any>, child: React.ReactNode) => void;
+  value: string;
+  label?: string
+  options?: Array<{key:string,value:string}>
+};
+
+export default function SelectBox({onChange,value,label,options}:Props):JSX.Element {
+ 
+  return (
+    <FormControl>
+      <InputLabel>{label}</InputLabel>
+      <Select value={value} onChange={onChange} label='エリア'>
+        {options?.map((option) => {
+          return  (<MenuItem value={option.value} key={option.value}>
+            {option.key}
+          </MenuItem>)
+        })}
+      </Select>
+    </FormControl>
+  );
+}
