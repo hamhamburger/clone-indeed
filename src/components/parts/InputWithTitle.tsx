958694c1f909edb8f2e2f50ad820ed2358ae839d
hamham <ham@example.com>
2022-09-15 22:41:48 +0900

    コードの修正

--- src/components/parts/InputWithTitle.tsx
+++ src/components/parts/InputWithTitle.tsx
@@ -1,3 +1,5 @@
+// こっちのコンポーネントはUIライブラリなしで作ったものです
+
 import { css } from '@emotion/react';
 
 type Props = {
@@ -7,45 +9,46 @@ type Props = {
   valueFromParent?: string;
   onSelect?: () => void;
   onBlur?: () => void;
-  onChange:(e:React.ChangeEvent<HTMLInputElement>) => void
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const Style = css({
+  display: 'flex',
+  alignItems: 'center',
+});
 
-  display: "flex",
-  alignItems: "center"
-})
-
-const InputWithTitle = ({ title, placeholder,valueFromParent, children,onSelect,onBlur,onChange}: Props):JSX.Element=> {
-
+const InputWithTitle = ({
+  title,
+  placeholder,
+  valueFromParent,
+  children,
+  onSelect,
+  onBlur,
+  onChange,
+}: Props): JSX.Element => {
   return (
-    
-      <div css={Style}>
-        <div css={css({marginLeft:"10px"})}>{title}</div>
+    <div css={Style}>
+      <div css={css({ marginLeft: '10px' })}>{title}</div>
       <input
         placeholder={placeholder}
         value={valueFromParent}
         css={css({
-          border: "0",
-          display: "block",
-          flex: "auto",
-          height: "30px",
-          outline: "none"
+          border: '0',
+          display: 'block',
+          flex: 'auto',
+          height: '30px',
+          outline: 'none',
         })}
         onSelect={onSelect}
         onBlur={onBlur}
         onChange={(e) => onChange(e)}
-        // onChange={(e) =>  onChange(e)}
-        />
-        {children}
-      </div>
-      
-    
-  )
-}
-
+      />
+      {children}
+    </div>
+  );
+};
 
 // このコンポーネント単品でも使えるようにしつつSelectableInputから子コンポーネントとして使われた場合にも値を渡せるようにする設計にするつもりだったが無理だ
 // 親コンポーネントでステート管理される設計にします
 
-export default InputWithTitle
+export default InputWithTitle;
