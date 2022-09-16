a065168f776d1145128ce2a0b8ae51a35bc7b18b
hamham <ham@example.com>
2022-09-16 17:17:10 +0900

    hotpeppr apiからジャンルデータを取得しフォームで扱いやすい形に整形

---
+++ src/lib/genreConditionData.js
@@ -0,0 +1,23 @@
+const genreConditionData = {
+  title: 'ジャンル',
+  name:"genre"
+  data: 
+   [ { key: 'G001', value: '居酒屋' },
+     { key: 'G002', value: 'ダイニングバー・バル' },
+     { key: 'G003', value: '創作料理' },
+     { key: 'G004', value: '和食' },
+     { key: 'G005', value: '洋食' },
+     { key: 'G006', value: 'イタリアン・フレンチ' },
+     { key: 'G007', value: '中華' },
+     { key: 'G008', value: '焼肉・ホルモン' },
+     { key: 'G017', value: '韓国料理' },
+     { key: 'G009', value: 'アジア・エスニック料理' },
+     { key: 'G010', value: '各国料理' },
+     { key: 'G011', value: 'カラオケ・パーティ' },
+     { key: 'G012', value: 'バー・カクテル' },
+     { key: 'G013', value: 'ラーメン' },
+     { key: 'G016', value: 'お好み焼き・もんじゃ' },
+     { key: 'G014', value: 'カフェ・スイーツ' },
+      { key: 'G015', value: 'その他グルメ' }]
+}
+     export default genreConditionData
